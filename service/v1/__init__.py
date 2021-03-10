bb4152030c5a58bd1a96b2642c5df84e6efd9e69
Willy Scheibel <willy.scheibel@hpi.de>
2021-03-10 18:28:07 +0100

    Start endpoints for image conversion

--- service/v1/__init__.py
+++ service/v1/__init__.py
@@ -17,7 +17,7 @@ router = APIRouter()
 
 
 GLRAW_DIRECTORY = os.environ.get('GLRAW_DIRECTORY', '')
-RESULT_DIR = os.environ.get('RESULT_DIR', '/data/results')
+RESULT_DIR = os.environ.get('RESULT_DIR', '/data/share')
 WORKING_DIRECTORY = '/opt/dependencies/glraw/'
 
 
@@ -41,3 +41,61 @@ async def get_root():
 @router.head("/", tags=[""])
 async def head_root(response: Response):
     response.status_code = status.HTTP_200_OK
+
+
+# CONVERSION TO RAW
+
+@router.post('/get_raw', tags=["conversion"])
+async def get_raw(background_tasks: BackgroundTasks, image_file: UploadFile = File(...)):
+    basename, extension = os.path.splitext(image_file.filename)
+
+    image_f, image_f_name = tempfile.mkstemp(suffix=extension, text=False)
+    with open(script_f, 'w+b') as f:
+        shutil.copyfileobj(image_file.file, f)
+
+    glraw_binary = os.path.join(GLRAW_DIRECTORY, 'glraw-cmd')
+    arguments = [ glraw_binary, "-o", RESULT_DIR, "-r", image_f_name ]
+
+    try:
+        print(' '.join(arguments), flush=True)
+        p = subprocess.run(arguments, capture_output=True, check=True, cwd=WORKING_DIRECTORY)
+    except:
+        print(p.stdout.decode("utf-8"), flush=True)
+        print(p.stderr.decode("utf-8"), flush=True)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=p.stderr.decode("utf-8"),
+        )
+
+    background_tasks.add_task(remove_temporary_files, [image_f], [image_f_name])
+
+    return FileResponse(image_f_name, filename=image_file.filename, media_type=image_file.content_type)
+
+
+# CONVERSION TO GLRAW
+
+@router.post('/get_glraw', tags=["conversion"])
+async def get_glraw(background_tasks: BackgroundTasks, image_file: UploadFile = File(...)):
+    basename, extension = os.path.splitext(image_file.filename)
+
+    image_f, image_f_name = tempfile.mkstemp(suffix=extension, text=False)
+    with open(script_f, 'w+b') as f:
+        shutil.copyfileobj(image_file.file, f)
+
+    glraw_binary = os.path.join(GLRAW_DIRECTORY, 'glraw-cmd')
+    arguments = [ glraw_binary, "-o", RESULT_DIR, image_f_name ]
+
+    try:
+        print(' '.join(arguments), flush=True)
+        p = subprocess.run(arguments, capture_output=True, check=True, cwd=WORKING_DIRECTORY)
+    except:
+        print(p.stdout.decode("utf-8"), flush=True)
+        print(p.stderr.decode("utf-8"), flush=True)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=p.stderr.decode("utf-8"),
+        )
+
+    background_tasks.add_task(remove_temporary_files, [image_f], [image_f_name])
+
+    return FileResponse(image_f_name, filename=image_file.filename, media_type=image_file.content_type)
