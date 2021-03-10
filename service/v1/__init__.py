ede8cc2fc19df92c3581799585fe3d4b831d220d
Willy Scheibel <willy.scheibel@hpi.de>
2021-03-10 23:54:24 +0100

    Binding of glraw to API

--- service/v1/__init__.py
+++ service/v1/__init__.py
@@ -25,7 +25,8 @@ def remove_temporary_files(file_ids, file_names):
     for f, f_name in zip(file_ids, file_names):
         print("Remove", f_name, flush=True)
         try:
-            os.close(f)
+            if f:
+                os.close(f)
             os.remove(f_name)
         except:
             pass
@@ -50,26 +51,41 @@ async def get_raw(background_tasks: BackgroundTasks, image_file: UploadFile = Fi
     basename, extension = os.path.splitext(image_file.filename)
 
     image_f, image_f_name = tempfile.mkstemp(suffix=extension, text=False)
-    with open(script_f, 'w+b') as f:
+    with open(image_f, 'w+b') as f:
         shutil.copyfileobj(image_file.file, f)
 
     glraw_binary = os.path.join(GLRAW_DIRECTORY, 'glraw-cmd')
     arguments = [ glraw_binary, "-o", RESULT_DIR, "-r", image_f_name ]
 
+    result_file = ''
+
     try:
         print(' '.join(arguments), flush=True)
         p = subprocess.run(arguments, capture_output=True, check=True, cwd=WORKING_DIRECTORY)
-    except:
-        print(p.stdout.decode("utf-8"), flush=True)
-        print(p.stderr.decode("utf-8"), flush=True)
+
+        list_of_strings = [x.decode('utf-8').rstrip('\n') for x in iter(p.stderr.splitlines())]
+
+        created_files = [ l[0:-len(" created.")] for l in list_of_strings if l.endswith(" created.") ]
+        
+        if len(created_files) != 1:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=e.stderr.decode("utf-8"),
+            )
+        
+        result_file = os.path.join(RESULT_DIR, created_files[0])
+        result_basename, result_extension = created_files[0].split(".", 1)
+    except subprocess.CalledProcessError as e:
+        print(e.stdout.decode("utf-8"), flush=True)
+        print(e.stderr.decode("utf-8"), flush=True)
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=p.stderr.decode("utf-8"),
+            detail=e.stderr.decode("utf-8"),
         )
 
-    background_tasks.add_task(remove_temporary_files, [image_f], [image_f_name])
+    background_tasks.add_task(remove_temporary_files, [image_f, None], [image_f_name, result_file])
 
-    return FileResponse(image_f_name, filename=image_file.filename, media_type=image_file.content_type)
+    return FileResponse(result_file, filename=basename+"."+result_extension, media_type='application/octet-stream')
 
 
 # CONVERSION TO GLRAW
@@ -85,17 +101,32 @@ async def get_glraw(background_tasks: BackgroundTasks, image_file: UploadFile = 
     glraw_binary = os.path.join(GLRAW_DIRECTORY, 'glraw-cmd')
     arguments = [ glraw_binary, "-o", RESULT_DIR, image_f_name ]
 
+    result_file = ''
+
     try:
         print(' '.join(arguments), flush=True)
         p = subprocess.run(arguments, capture_output=True, check=True, cwd=WORKING_DIRECTORY)
-    except:
-        print(p.stdout.decode("utf-8"), flush=True)
-        print(p.stderr.decode("utf-8"), flush=True)
+
+        list_of_strings = [x.decode('utf-8').rstrip('\n') for x in iter(p.stderr.splitlines())]
+
+        created_files = [ l[0:-len(" created.")] for l in list_of_strings if l.endswith(" created.") ]
+        
+        if len(created_files) != 1:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=e.stderr.decode("utf-8"),
+            )
+        
+        result_file = os.path.join(RESULT_DIR, created_files[0])
+        result_basename, result_extension = created_files[0].split(".", 1)
+    except subprocess.CalledProcessError as e:
+        print(e.stdout.decode("utf-8"), flush=True)
+        print(e.stderr.decode("utf-8"), flush=True)
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=p.stderr.decode("utf-8"),
+            detail=e.stderr.decode("utf-8"),
         )
 
-    background_tasks.add_task(remove_temporary_files, [image_f], [image_f_name])
+    background_tasks.add_task(remove_temporary_files, [image_f, None], [image_f_name, result_file])
 
-    return FileResponse(image_f_name, filename=image_file.filename, media_type=image_file.content_type)
+    return FileResponse(result_file, filename=basename+"."+result_extension, media_type='application/octet-stream')
